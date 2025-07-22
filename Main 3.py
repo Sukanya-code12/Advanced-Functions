b502dc070f703547a236ae2c9ddad81685300e6a
Sukanya-Code12 <sukanyahiya2012@gmail.com>
2025-07-22 18:34:17 +0530

    Advanced Functions

---
+++ Main 3.py
@@ -0,0 +1,3 @@
+number=[11,12,13,14,15,16]
+even=[x for x in number if x%2==0]
+print("The even numbers are", even)
---
+++ Main 4.py
@@ -0,0 +1,2 @@
+dictionary={str(x): x**2 for x in [1,2,3,4,5,]}
+print(dictionary)
